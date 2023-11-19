79b0b430c610fca1689e47337e4cf32ea14d21c4
ManOSan96 <149111901+ManOSan96@users.noreply.github.com>
2023-11-20 01:49:09 +0200

    Add files via upload

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,12 @@
+export default function Tabs({ children, buttons, buttonsContainer }) {
+    const ButtonsContainer = buttonsContainer;
+    return (
+      <>
+      <ButtonsContainer>
+        {buttons}
+      </ButtonsContainer>
+      {children}
+      </>
+
+    )
+}
